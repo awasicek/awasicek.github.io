f80107afc309f696eeca74e71da9f99e079cf76e
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-02-01 11:59:42 -0800

    added gameboard

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 
+$('#game-hex-1 *').click(function(){console.log('clicked game-hex-1')})
+$('#game-hex-2 *').click(function(){console.log('clicked game-hex-2')})
+
+
+for (i=1; i<45; i+=1) {
+  $('#game-hex-' + i + ' *').click(function(){console.log('clicked game-hex-' + i)})
+}
